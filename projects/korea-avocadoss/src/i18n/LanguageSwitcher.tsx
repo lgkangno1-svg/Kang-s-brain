0647033b43ccd052449d92c80ee0bf1e4264e2e4
lgkangno1-svg <lg.kangno1@gmail.com>
2026-08-26 19:30:15 +0900

    i18n: add accessible locale switcher

---
+++ projects/korea-avocadoss/src/i18n/LanguageSwitcher.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import {useLocale, useTranslations} from 'next-intl';
+import {ChangeEvent, useTransition} from 'react';
+
+import {P0_LOCALES, getLocaleDefinition} from '@/lib/i18n/locales';
+import {usePathname, useRouter} from './navigation';
+
+export function LanguageSwitcher() {
+  const locale = useLocale();
+  const t = useTranslations('Global');
+  const pathname = usePathname();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  function onChange(event: ChangeEvent<HTMLSelectElement>) {
+    const nextLocale = event.target.value as (typeof P0_LOCALES)[number];
+    startTransition(() => {
+      router.replace(pathname, {locale: nextLocale});
+    });
+  }
+
+  return (
+    <label className="languageSwitcher">
+      <span className="srOnly">{t('language')}</span>
+      <select
+        aria-label={t('language')}
+        value={locale}
+        onChange={onChange}
+        disabled={isPending}
+      >
+        {P0_LOCALES.map((code) => {
+          const definition = getLocaleDefinition(code);
+          return <option key={code} value={code}>{definition.nativeName}</option>;
+        })}
+      </select>
+    </label>
+  );
+}
